08bc8ded0c43efdf6e800165e2c9e1e403861f49
JGnogueira87 <147269195+JGnogueira87@users.noreply.github.com>
2023-11-04 11:10:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,82 @@
-distance = 25000;
+/*distance = 25000;
 time = 710;
 gender = "male";
 pushUps = 40;
 abs = 35;
+*/
 
-function distance (val){
-  return val >= 2100
+function verifyDistance (gender, distance){
+  if (gender === "male"){
+    if (distance >= 2400){
+      return true
+  }
+  return false
+} else{
+    if (distance >= 2400){
+      return true
+  }
+  return false
+}
 }
 
-function time (val){
-  return val <= 720
+
+function verifyTime (gender, time){
+  if (gender === "male"){
+  if (time <= 720){
+    return true
+  }
+  return false
+} else{
+  if (time <= 840){
+    return true
+}
+return false
+}
 }
 
-function pushUps (val){
-  return val >= 40
+
+function verifyPushUps (gender, pushUps){
+  if (gender === "male"){
+    if (pushUps >= 30){
+      return true
+  }
+  return false
+} else{
+    if (pushUps >= 20)
+      return true
+  }
+  return false
 }
 
-function abs (val){
-  return val >= 35
+
+function verifyAbs (gender, abs){
+  if (gender === "male"){
+    if (abs >= 35){
+      return true
+  }
+  return false
+} else{
+    if (abs >= 30){
+      return true
+  }
+  return false
 }
+}
+
 
 function testTAF(distance, time, gender, pushUps, abs) {
   passed = false;
 
-  if (distance(val) && time(val) && gender === 'male' && pushUps(val) && abs(val)) {
+  if (verifyDistance (gender, distance) && verifyTime (gender, time) && verifyPushUps (gender, pushUps) && verifyAbs (gender, abs)) {
     passed = true;
-  } else if (distance(val) && time(val) && gender === 'female' && pushUps(val) && abs(val)) {
-    passed = true;
-  }
-
+  } 
   return passed;
 }
 
-console.log(
-  "Candidato passou no teste?: \nresposta: ",
+// console.log(
+//   "Candidato passou no teste?: \nresposta: ",
 
-  testTAF(distance, time, gender, pushUps, abs)
-);
+//   testTAF(distance, time, gender, pushUps, abs)
+// );
 
 module.exports = testTAF;
